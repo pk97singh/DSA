7a2827391591c3d2c1f9e2934c44159ad8563efd
Prabhat kumar <79047238+pk97singh@users.noreply.github.com>
2026-08-16 12:43:33 +0530

    Add function to find max sum of subarray
    Implement function to find maximum sum of subarray of size k.

---
+++ sliciding_Window/function_max_subarray.py
@@ -0,0 +1,25 @@
+my_lst= [100,200,300,400]
+def find_max_sum_subarray (k):
+    low=0
+    high=k-1 
+    n=len(my_lst)
+    res=0 
+    sum=0 
+    
+    for i in range(low ,high+1):
+        sum=sum+my_lst[i] #create our first window   
+        
+    while (high<n):
+        res=max(res,sum)
+        low=low+1 
+        high=high+1 
+        
+        if high==n:
+            break 
+        
+        sum=sum-my_lst[low -1 ]+my_lst[high]
+        
+        
+    return res 
+
+print(find_max_sum_subarray(2))
