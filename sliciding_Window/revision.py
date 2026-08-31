23b82f770e9c9db033ee3898ed3bdfa151171a29
Prabhat kumar <79047238+pk97singh@users.noreply.github.com>
2026-08-31 21:09:45 +0530

    Update revision.py

--- sliciding_Window/revision.py
+++ sliciding_Window/revision.py
@@ -25,6 +25,35 @@ while (h<n):
     sum=sum+arr[h]-arr[l-1]
    
 print(sum)
- 
+
+#quoestion 2.
+nums = [2,3,1,2,4,3]
+target = 7
+
+#variable , 
+h=0
+l=0
+n=len(nums)
+sum=0
+res=float('inf')
+
+while(h<n):
+    sum=sum+nums[h]
+    h=h+1
+    
+    
+    while(sum>=target):  
+        len=h-l
+        res=min(res,len)
+        sum=sum-nums[l]
+        l=l+1
+        
+if res==float('inf'):
+        print(0)
+         
+print(res)
+
+         
+
     
     
