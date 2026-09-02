d7972bf8e364d1231da415d579487a22b05f44ce
Prabhat kumar <79047238+pk97singh@users.noreply.github.com>
2026-09-02 19:53:41 +0530

    Create reverse.py

---
+++ stack/reverse.py
@@ -0,0 +1,15 @@
+def reverse_string(s):
+    stack=[]
+    n=len(s)
+    res=""
+    
+    for i in range(0,n):
+        stack.append(s[i])
+        
+    while (stack):
+        c=stack.pop()
+        res+=c
+        
+    return res 
+
+print(reverse_string("hello"))
