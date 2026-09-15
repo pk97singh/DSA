6e0845bcc2328e4d98b6ee92e8d93d6e564e1b40
Prabhat kumar <79047238+pk97singh@users.noreply.github.com>
2026-09-15 20:21:50 +0530

    Update common.py

--- stack/revision/common.py
+++ stack/revision/common.py
@@ -20,3 +20,30 @@ for ch in s1:
     
 if len(stack)==0:
         print("true")
+
+#decode
+def decode(s1):
+    stack=[]
+    current=""
+    num=0 
+    for ch in s1:
+        if ch.isdigit():
+            num=num*10+int(ch)
+        
+        elif ch=='[':
+            stack.append((current,num))
+            current=""
+            num=0
+            
+        elif ch==']':
+            prev,count=stack.pop()
+            current=prev+current*count 
+
+            
+        else:
+            current+=ch
+            
+    return current 
+
+
+print(decode("3[a2[b]]"))
